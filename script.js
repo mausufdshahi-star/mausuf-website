e92ed12a0778689b8f01e2a4971c510fee029225
mausufdshahi-star <mausufdshahi@gmail.com>
2026-03-13 22:53:30 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const toggle = document.getElementById('menu-toggle');
+  const nav = document.getElementById('nav');
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+</script>
