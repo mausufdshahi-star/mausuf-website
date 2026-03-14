2fe80a3f97ab48a09fa1e2b33d4fc482ebcb5dfb
mausufdshahi-star <mausufdshahi@gmail.com>
2026-03-14 18:17:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-<script>
-  const toggle = document.getElementById('menu-toggle');
-  const nav = document.getElementById('nav');
 
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-</script>
