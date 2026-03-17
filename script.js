3b41585a18994856fcf3d7eeff197bb7c8e2426b
mausufdshahi-star <mausufdshahi@gmail.com>
2026-03-17 22:19:07 +0545

    eng-nep js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let isNepali = false;
 
+function toggleLanguage() {
+  isNepali = !isNepali;
+
+  document.querySelectorAll("[data-en]").forEach((el) => {
+    el.textContent = isNepali
+      ? el.getAttribute("data-ne")
+      : el.getAttribute("data-en");
+  });
+}
