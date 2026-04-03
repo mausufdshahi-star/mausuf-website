63323b4b397dec41050178dbdd17785e33c86c1d
mausufdshahi-star <mausufdshahi@gmail.com>
2026-04-03 18:19:40 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let isNepali = false;
 
-function toggleLanguage() {
-  isNepali = !isNepali;
-
-  document.querySelectorAll("[data-en]").forEach((el) => {
-    el.textContent = isNepali
-      ? el.getAttribute("data-ne")
-      : el.getAttribute("data-en");
-  });
-}
